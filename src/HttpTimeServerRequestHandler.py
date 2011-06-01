a5c28d69bb9eef9ee12ead89094087ea21f8b5f9
matthias <schmid_mat@gmx.at>
2011-06-01 11:44:41 +0200

    changed requesthandler

--- src/HttpTimeServerRequestHandler.py
+++ src/HttpTimeServerRequestHandler.py
@@ -12,10 +12,9 @@ class HttpTimeServerRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     Class Server
     '''
     def do_GET(self):
-        try:
-            self.send_response(200)
-            self.send_header('Content-type',    'text/plain')
-            self.end_headers()
-            self.wfile.write(SystemClock.getUTCTime())
+        self.send_response(200)
+        self.send_header('Content-type',    'text/plain')
+        self.end_headers()
+        self.wfile.write(SystemClock.getUTCTime())
 
     
